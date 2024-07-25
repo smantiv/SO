e54191d68f27a828f9191b7ac2192d477dc1cba6
smantiv <sofiamantilla@javeriana.edu.co>
2024-07-24 22:25:24 -0500

    Add files via upload

---
+++ example0.c
@@ -0,0 +1,31 @@
+/********************************************************
+Autor: S. Mantilla
+Materia: Sistemas Operativos
+Fecha: 18-07-2024
+Tema: Introduccion al lenguaje C y los entornos
+  -Entornos en la web (CLOUD/NUBE)
+  -Comandos prinvcuiplaes de Linux
+  -Programa 0: hola mundo
+********************************************************/
+
+#include <stdio.h>
+
+int main(int argc, char *argv[]){
+  
+  printf("°❀⋆.ೃ࿔*:･°❀⋆.ೃ࿔*:･°❀⋆.ೃ࿔*:･°❀⋆.ೃ࿔*°❀⋆.ೃ࿔*:･°❀⋆.ೃ࿔*:･\n");
+  printf("     Hola soy un PROFESIONAL EN FORMACIÓN\n");
+  printf("                  (>ᴗ•) !\n");
+  printf("°❀⋆.ೃ࿔*:･°❀⋆.ೃ࿔*:･°❀⋆.ೃ࿔*:･°❀⋆.ೃ࿔*°❀⋆.ೃ࿔*:･°❀⋆.ೃ࿔*:･\n\n");
+  printf("Nombre: Sofia Mantilla\n\n");
+  printf("Materias que curso:\n");
+  printf("˚ʚ♡ɞ˚Sistemas Operativos\n");
+  printf("˚ʚ♡ɞ˚Estructuras de datos\n");
+  printf("˚ʚ♡ɞ˚Fundamentos de Software\n\n");
+  printf("Carrera: Ingeniería de sistemas♡\n\n");
+  printf("me gustan los gatitos\n\n");
+
+  
+  return 0;
+}
+
+
