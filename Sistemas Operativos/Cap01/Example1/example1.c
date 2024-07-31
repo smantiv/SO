24b863ed5891ae397eee59a579823f381fc971b5
smantiv <sofiamantilla@javeriana.edu.co>
2024-07-30 21:00:29 -0500

    Create example1.c

---
+++ Sistemas Operativos/Cap01/Example1/example1.c
@@ -0,0 +1,19 @@
+/********************************************************
+Autor: S. Mantilla
+Materia: Sistemas Operativos
+Fecha: 25-07-2024
+Tema: Entrada y salida en C
+********************************************************/
+#include <stdio.h>
+#include <math.h>
+#include "funciones.h"
+
+int main(int argc, char *argv[]){
+  suma();
+  areaCirculo();
+  vol();
+  volC();
+  hipo();
+
+  return 0;
+}
