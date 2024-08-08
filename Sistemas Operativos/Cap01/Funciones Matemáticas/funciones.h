b3116bab98af10ce6d6a2f2790649607d603686a
smantiv <sofiamantilla@javeriana.edu.co>
2024-08-08 10:50:32 -0500

    Create funciones.h

---
+++ Sistemas Operativos/Cap01/Funciones Matemáticas/funciones.h
@@ -0,0 +1,24 @@
+/********************************************************
+Autor: S. Mantilla
+Materia: Sistemas Operativos
+Fecha: 01-08-2024
+Tema: Ejercicio funciones mátematicas
+********************************************************/
+
+#include <stdio.h>
+#include <math.h>
+
+/* Calcular suma */
+void suma();
+
+/* Calcular el área de un círculo ingresando el radio por teclado */
+void areaCirculo();
+
+/* Calcular el volumen de circunferencia ingresando el radio por teclado */
+void vol();
+
+/* Calcular el volumen de un cono ingresando variables necesarias por teclado */
+void volC();
+
+/* Calcular una hipotenusa ingresando variables necesarias por teclado */
+void hipo();
