b696e907fc6c7fe4315d7db348215237d7d7470b
smantiv <sofiamantilla@javeriana.edu.co>
2024-10-17 17:04:29 -0500

    Update TallerProcesosHeader.h

--- Sistemas Operativos/Cap01/Taller Inicial Fork/TallerProcesosHeader.h
+++ Sistemas Operativos/Cap01/Taller Inicial Fork/TallerProcesosHeader.h
@@ -10,16 +10,16 @@ Descripción: Header para TallerProcesosFunciones.c.
 #ifndef TALLER_PROCESOS_HEADER_H
 #define TALLER_PROCESOS_HEADER_H
 
-// Función para leer un arreglo de enteros desde un archivo
-// nombre_archivo: nombre del archivo a leer
-// cantidad_elementos: número de enteros a leer
-// Retorna un puntero a un arreglo dinámico de enteros
+//función para leer un arreglo de enteros desde un archivo
+//nombre_archivo: nombre del archivo a leer
+//cantidad_elementos: número de enteros a leer
+//retorna un puntero a un arreglo dinámico de enteros
 int* leer_arreglo(const char* nombre_archivo, int cantidad_elementos);
 
-// Función para calcular la suma de un arreglo de enteros
-// arreglo: puntero al arreglo de enteros
-// cantidad_elementos: número de elementos en el arreglo
-// Retorna la suma de los enteros en el arreglo
+//función para calcular la suma de un arreglo de enteros
+//arreglo: puntero al arreglo de enteros
+//cantidad_elementos: número de elementos en el arreglo
+//retorna la suma de los enteros en el arreglo
 int suma_arreglo(int* arreglo, int cantidad_elementos);
 
 #endif
